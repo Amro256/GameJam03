a0ee5bbb30d50a040a259d0af87f6147b2148edc
Amro <ae7630w@gre.ac.uk>
2025-03-08 17:13:04 +0000

    Added timer
    Level timer added

--- Assets/Scripts/UI Manager.cs
+++ Assets/Scripts/UI Manager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -9,18 +10,35 @@ public class NewBehaviourScript : MonoBehaviour
 
     [SerializeField] Image canvasImage; //Refernece to the Image component of the canvas -- Make this Private after testing)
 
+    [SerializeField] TMP_Text timerText;
+    [SerializeField] float timeLeft = 20f;
+    private bool isTimerRunning = false;
 
     // Start is called before the first frame update
     void Start()
     {
         //Method here that will randomise the final item
         randomItem();
+        isTimerRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isTimerRunning)
+        {
+            if(timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                displayTimer(timeLeft);
+            }
+            else
+            {
+                Debug.Log("You failed!");
+                timeLeft = 0f;
+                isTimerRunning = false;
+            }
+        }
     }
     
     private void randomItem()
@@ -42,7 +60,13 @@ public class NewBehaviourScript : MonoBehaviour
         {
             canvasImage.sprite = sr.sprite;
         }
+    }
 
-        
+    void displayTimer(float time)
+    {
+        float seconds = Mathf.FloorToInt(time % 60);
+        float mileseconds = (time % 1) * 100;
+
+        timerText.text = string.Format("{0:00} : {1:00}", seconds, mileseconds); 
     }
 }
